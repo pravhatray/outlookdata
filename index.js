3c766f34c1e70b4804d18152a52cf3cddd6e4b50
Pravhat Ray <pravhatray@gmail.com>
2022-11-21 10:17:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const { exec } = require('child_process');
+
+// Set port (default: 3000). For Heroku, we need to use
+// the port set by the environment variable $PORT
+require('dotenv').config();
+const PORT = process.env.PORT || 8000;
+
+const command = `json-server --watch db.json --port ${PORT}`;
+
+exec(command, (err, stdout, stderr) => {
+  if (err) {
+    console.log('Error running exec', err);
+    return;
+  }
+  console.log('stdout:', stdout);
+  console.log('stderr:', stderr);
+});
